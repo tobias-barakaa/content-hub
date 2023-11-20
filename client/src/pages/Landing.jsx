62b2dc3c30cbf2eb04ac7c48b61a08a1e4e30fe5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-20 09:57:53 +0300

    add

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -14,15 +14,19 @@ const Landing = () => {
       <div class="container-class">
   <div className='left' >
   <span>
-      <div className='expand-online-presence'>
-        Expand your online presence
+      <div className='expand'>
+        Expand your <br /> online presence
       </div>
-      <div>
-        with our expert social media strategies,
-      </div>
-      <div>
+      <div className='social'>
+      <span className='implement'>Implementing</span> expert social media strategies,
+      creating captivating blog content, and producing <br />
+      high-quality articles to attract audience,
+        captivating blog content, and high-quality articles. <br />
         captivating blog content, and high-quality articles.
+
+        
       </div>
+        
     </span>
     <div>
       <Link to='/register' class='buttonn'>CLICK TO GET STARTED</Link>
