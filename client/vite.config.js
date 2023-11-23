8c5542a2cdd75116bb26a1af7ed4a14970b690f3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-23 14:42:11 +0300

    finished

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000/api',
+        target: 'http://52.86.222.134/:4000/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import cookieParser from 'cookie-parser';
 
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 // routers;
 import articleRouter from './routes/articleRouter.js';
